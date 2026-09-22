cca4b7c8afc00adf6b4c3c33b9b90ccd2e67b85a
vikasmanral <vikasmanral9876@gmail.com>
2026-09-22 18:08:44 +0530

    feat: integrate AI APIs and style home and interview pages

--- Frontend/src/app.routes.jsx
+++ Frontend/src/app.routes.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router";
 import Login from "./features/auth/pages/Login";
 import Register from "./features/auth/pages/Register";
 import Protected from "./features/auth/components/Protected";
+import Home from "./features/interview/pages/Home";
 
 
 export const router = createBrowserRouter([
@@ -15,6 +16,6 @@ export const router = createBrowserRouter([
     }, 
     {
         path:"/",
-        element:<Protected><h1>Home Page</h1></Protected>
+        element:<Protected><Home /></Protected>
     }
 ])
--- Frontend/src/features/auth/hooks/useAuth.js
+++ Frontend/src/features/auth/hooks/useAuth.js
@@ -45,11 +45,16 @@ export const useAuth = () => {
 
   useEffect(() => {
     const getAndSetUser = async () => {
-      const data = await getMe();
-      setUser(data.user);
-      setLoading(false);
+      try {
+        const data = await getMe();
+        setUser(data.user);
+      } catch (err) {
+        console.log(err)
+      } finally {
+        setLoading(false);
+      }
     };
-    getAndSetUser()
+    getAndSetUser();
   }, []);
   return { user, loading, handleRegister, handleLogin, handleLogout };
 };
